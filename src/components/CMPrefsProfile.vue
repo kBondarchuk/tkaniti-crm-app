e46346db3270f8bad5eacfbf72a38de15f55b64b
Konstantin Bondarchuk <dev@bondarchuk.ru>
2023-02-27 12:13:37 +0700

    Profile Redesign

--- src/components/CMPrefsProfile.vue
+++ src/components/CMPrefsProfile.vue
@@ -1,59 +1,60 @@
 <template>
-  <WPanelContainer size="full" style="height: 100%">
-    <WPanel size="22em" style="border-right: 1px solid rgba(34, 36, 38, 0.15)">
-      <!-- Form -->
-      <form class="ui form">
-        <UITextfield v-model.trim.lazy="user.username">Имя</UITextfield>
-        <!-- <UITextfield v-model.trim.lazy="user.email" readonly>E-Mail</UITextfield> -->
-        <UITextfield v-model.trim.lazy="user.password">Пароль</UITextfield>
+  <div style="height: 100%">
+    <WPanelContainer size="full" style="height: 100%">
+      <WPanel size="22em" style="border-right: 1px solid rgba(34, 36, 38, 0.15)">
+        <!-- Form -->
+        <form class="ui form">
+          <UITextfield v-model.trim.lazy="user.username">Имя</UITextfield>
+          <!-- <UITextfield v-model.trim.lazy="user.email" readonly>E-Mail</UITextfield> -->
+          <UITextfield v-model.trim.lazy="user.password">Пароль</UITextfield>
 
-        <!-- <div class="field">
+          <!-- <div class="field">
         <div class="ui sub header">Права</div>
         <select name="rights" id="rights_dropdown" multiple class="ui fluid dropdown">
           <option value>Права</option>
           <option :value="item" v-for="item in authRights" :key="item" selected>{{ item }}</option>
       </select>-->
 
-        <!-- <a class="ui label" v-for="item in authRights" :key="item">
+          <!-- <a class="ui label" v-for="item in authRights" :key="item">
           {{ item }}
           <i class="delete icon"></i>
       </a>-->
-        <!-- </div> -->
-        <div class="ui hidden divider"></div>
-        <input
-          name="submit"
-          type="submit"
-          value="Сохранить"
-          class="ui small compact primary submit button"
-          @click.prevent="updateUser"
-        />
-        <div class="ui hidden divider"></div>
-      </form>
-    </WPanel>
+          <!-- </div> -->
+          <div class="ui hidden divider"></div>
+          <input
+            name="submit"
+            type="submit"
+            value="Сохранить"
+            class="ui small compact primary submit button"
+            @click.prevent="updateUser"
+          />
+          <div class="ui hidden divider"></div>
+        </form>
+      </WPanel>
 
-    <!-- Second Column -->
-    <!-- <div class="ten wide column"> -->
-    <WPanel size="full">
-      <!-- info -->
-      <table class="ui compact small collapsing table" style="border: 0">
-        <tbody>
-          <tr>
-            <td>E-Mail:</td>
-            <td>{{ user.email }}</td>
-          </tr>
-          <tr>
-            <td>Права:</td>
-            <td>
-              <UIBadge
-                v-for="item in authRights"
-                :key="item"
-                :text="item"
-                style="margin: 0.2rem 0.25rem"
-                :value="item"
-              />
-            </td>
-          </tr>
-          <!-- <tr>
+      <!-- Second Column -->
+      <!-- <div class="ten wide column"> -->
+      <WPanel size="full">
+        <!-- info -->
+        <table class="ui compact small collapsing table" style="border: 0">
+          <tbody>
+            <tr>
+              <td>E-Mail:</td>
+              <td>{{ user.email }}</td>
+            </tr>
+            <tr>
+              <td>Права:</td>
+              <td>
+                <UIBadge
+                  v-for="item in authRights"
+                  :key="item"
+                  :text="item"
+                  style="margin: 0.2rem 0.25rem"
+                  :value="item"
+                />
+              </td>
+            </tr>
+            <!-- <tr>
             <td>Филиалы:</td>
             <td>{{ user.branches }}</td>
           </tr>
@@ -61,20 +62,20 @@
             <td>Инвесторы:</td>
             <td>{{ user.investors }}</td>
           </tr> -->
-          <tr>
-            <td>Дата создания:</td>
-            <td>{{ $filters.date(user.created_ts) }}</td>
-          </tr>
-          <tr>
-            <td>Последнее обновление:</td>
-            <td>{{ $filters.date(user.updated_ts) }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </WPanel>
-  </WPanelContainer>
-  <!-- grid -->
-  <!-- </div> -->
+            <tr>
+              <td>Дата создания:</td>
+              <td>{{ $filters.date(user.created_ts) }}</td>
+            </tr>
+            <tr>
+              <td>Последнее обновление:</td>
+              <td>{{ $filters.date(user.updated_ts) }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </WPanel>
+    </WPanelContainer>
+    <!-- grid -->
+  </div>
 </template>
 
 <script>
