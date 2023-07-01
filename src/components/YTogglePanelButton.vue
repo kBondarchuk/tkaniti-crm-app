63d7cc83b5626e3462f108c551ceef4b21235292
Konstantin Bondarchuk <dev@bondarchuk.ru>
2023-07-01 18:31:25 +0700

    Add toggle button

---
+++ src/components/YTogglePanelButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <UIButton icon="filter" type="labeled basic" style="min-width: 10em">
+    <span v-if="show">Скрыть</span>
+    <span v-else>Показать</span>
+  </UIButton>
+</template>
+
+<script setup>
+/// SETUP
+
+/// PROPS
+
+const props = defineProps({
+  show: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+/// FUNCTIONS
+</script>
--- src/global_components.js
+++ src/global_components.js
@@ -9,7 +9,8 @@ import UIListItem from "@/components/UIListItem.vue";
 import UIListSection from "@/components/UIListSection.vue";
 import UITextLoader from "@/components/UITextLoader.vue";
 import UIDetailsTabs from "@/components/UIDetailsTabs.vue";
-import BackButton from "@/components/BackButton.vue";
+import YBackButton from "@/components/YBackButton.vue";
+import YTogglePanelButton from "@/components/YTogglePanelButton.vue";
 
 const register = (app) => {
   app.component("LayoutPage", LayoutPage);
@@ -23,7 +24,8 @@ const register = (app) => {
   app.component("UIListSection", UIListSection);
   app.component("UITextLoader", UITextLoader);
   app.component("UIDetailsTabs", UIDetailsTabs);
-  app.component("BackButton", BackButton);
+  app.component("YBackButton", YBackButton);
+  app.component("YTogglePanelButton", YTogglePanelButton);
 };
 
 export default {
