bb1642b9f679e2362e8529c79c4153d25abf4a4a
Konstantin Bondarchuk <dev@bondarchuk.ru>
2024-01-18 17:41:35 +0700

    Refactored for vue3

--- src/components/TKOrderPaymentMethod.vue
+++ src/components/TKOrderPaymentMethod.vue
@@ -2,34 +2,32 @@
   <div :class="status?.class"><i :class="status?.icon + ' icon'" :title="status?.text"></i></div>
 </template>
 
-<script>
-export default {
-  name: "TKOrderPaymentMethod",
+<script setup>
+import { computed } from "vue";
 
-  components: {},
+// name: "TKOrderPaymentMethod",
 
-  props: {
-    value: {
-      type: Number,
-      default: null,
-    },
+/// PROPS
+
+const props = defineProps({
+  value: {
+    type: Number,
+    default: null,
   },
+});
 
-  data() {
-    return {
-      statuses: [
-        { id: "0", icon: "", text: "", class: "text-color-red" },
-        { id: "1", icon: "credit card outline", text: "Карта", class: "text-color-blue" },
-        { id: "2", icon: "money ", text: "Перевод", class: "text-color-grey" },
-      ],
-    };
-  },
+/// COMPUTED
 
-  computed: {
-    status() {
-      const sid = 1;
-      return this.statuses.find((item) => item.id == this.value);
-    },
-  },
-};
+const status = computed(() => {
+  const sid = 1;
+  return statuses.find((item) => item.id == props.value);
+});
+
+/// DATA
+
+const statuses = [
+  { id: 0, icon: "", text: "", class: "text-color-red" },
+  { id: 1, icon: "credit card outline", text: "Карта", class: "text-color-blue" },
+  { id: 2, icon: "money ", text: "Перевод", class: "text-color-grey" },
+];
 </script>
