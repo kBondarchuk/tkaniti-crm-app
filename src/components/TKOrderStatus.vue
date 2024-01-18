67462ee6ae3df473a2de78dfd207ba4fbdbc4372
Konstantin Bondarchuk <dev@bondarchuk.ru>
2024-01-18 17:39:33 +0700

    Refactored to vue3

--- src/components/TKOrderStatus.vue
+++ src/components/TKOrderStatus.vue
@@ -2,37 +2,37 @@
   <UIBadge :text="status?.name" :class="status?.class" />
 </template>
 
-<script>
-export default {
-  name: "TKOrderStatus",
+<script setup>
+import { computed } from "vue";
 
-  props: {
-    value: {
-      type: Number,
-      default: null,
-    },
-  },
+// name: "TKOrderStatus",
 
-  data() {
-    return {
-      statuses: [
-        { id: "0", icon: "", name: "Новый", class: "background-color-red text-color-white" },
-        { id: "1", icon: "", name: "Проверка", class: "background-color-green text-color-white" },
-        { id: "2", icon: "", name: "Оплата", class: "background-color-blue text-color-white" },
-        { id: "7", icon: "", name: "Сборка", class: "background-color-teal text-color-white" },
-        { id: "3", icon: "", name: "К отправке", class: "background-color-purple text-color-white" },
-        { id: "4", icon: "", name: "Отправлен", class: "background-color-violet text-color-white" },
-        { id: "5", icon: "", name: "Получен", class: "background-color-grey text-color-white" },
-        { id: "6", icon: "", name: "Отменён", class: "" },
-      ],
-    };
-  },
+/// PROPS
 
-  computed: {
-    status() {
-      const sid = 1;
-      return this.statuses.find((item) => item.id == this.value);
-    },
+const props = defineProps({
+  value: {
+    type: Number,
+    default: null,
   },
-};
+});
+
+/// COMPUTED
+
+const status = computed(() => {
+  const sid = 1;
+  return statuses.find((item) => item.id == props.value);
+});
+
+/// DATA
+
+const statuses = [
+  { id: 0, icon: "", name: "Новый", class: "background-color-red text-color-white" },
+  { id: 1, icon: "", name: "Проверка", class: "background-color-green text-color-white" },
+  { id: 2, icon: "", name: "Оплата", class: "background-color-blue text-color-white" },
+  { id: 7, icon: "", name: "Сборка", class: "background-color-teal text-color-white" },
+  { id: 3, icon: "", name: "К отправке", class: "background-color-purple text-color-white" },
+  { id: 4, icon: "", name: "Отправлен", class: "background-color-violet text-color-white" },
+  { id: 5, icon: "", name: "Получен", class: "background-color-grey text-color-white" },
+  { id: 6, icon: "", name: "Отменён", class: "" },
+];
 </script>
