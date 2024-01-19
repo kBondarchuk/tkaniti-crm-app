57758fd983fe475cdf52812689d29b47d4086d97
Konstantin Bondarchuk <dev@bondarchuk.ru>
2024-01-19 08:57:25 +0700

    Add UIBadge

---
+++ src/components/UIStatusBadge.vue
@@ -0,0 +1,28 @@
+<template>
+  <UIBadge :text="status?.name" :class="status?.class" />
+</template>
+
+<script setup>
+// name: "UIStatusBadge"
+
+import { computed } from "vue";
+
+/// PROPS
+
+const props = defineProps({
+  value: {
+    type: Number,
+    default: null,
+  },
+  statuses: {
+    type: Array,
+    default: () => [],
+  },
+});
+
+/// COMPUTED
+
+const status = computed(() => {
+  return props.statuses.find((item) => item.id == props.value);
+});
+</script>
