9216b06619cb9fc27050cb33850645a5a64c6c50
Konstantin Bondarchuk <dev@bondarchuk.ru>
2024-06-05 11:21:03 +0300

    Добавлена статистика по заказам

--- src/components/TKCustomersDetailsTab_General.vue
+++ src/components/TKCustomersDetailsTab_General.vue
@@ -4,13 +4,20 @@
     <TKCustomerDetails :customer="customer" :is-loading="isLoading" />
 
     <!-- Second column -->
-    <!-- <template #second>
+    <template #second>
       <UIList :is-loading="isLoading">
+        <UIListSection header="Статистика заказов">
+          <UIListItem name="Всего заказов" :value="customer?.total_orders_count" />
+          <UIListItem name="Оплаченных" :value="customer?.paid_orders_count" />
+          <UIListItem name="Неоплаченных заказов" :value="customer?.unpaid_orders_count" />
+          <UIListItem name="Отмененных вручную" :value="customer?.cancelled_orders_count" />
+          <UIListItem name="Отмененных по тайм-ауту" :value="customer?.timeout_orders_count" />
+        </UIListSection>
         <UIListSection>
-          <UIListItem name="Кол-во неоплаченных заказов" :value="customer?.unpaid_orders_count" />
+          <UIListItem name="Процент невыкупленных" :value="raiting" />
         </UIListSection>
       </UIList>
-    </template> -->
+    </template>
   </LayoutTwoColumns>
   <!--  -->
 </template>
@@ -34,5 +41,18 @@ const props = defineProps({
 
 const isLoading = computed(() => {
   return props.customer == null;
+});
+
+const raiting = computed(() => {
+  if (props.customer == null) return null;
+
+  const percent =
+    ((props.customer.cancelled_orders_count + props.customer.timeout_orders_count) /
+      (props.customer.cancelled_orders_count +
+        props.customer.timeout_orders_count +
+        props.customer.paid_orders_count)) *
+    100;
+
+  return Number(percent).toFixed(1) + "%";
 });
 </script>
